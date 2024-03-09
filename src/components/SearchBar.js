5a0a50e04697e2d703559a7abb5376f5808a03ad
gamze <gamzekandara@gmail.com>
2024-03-09 16:34:48 -0500

    Adds event handler into SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 function SearchBar() {
+  const [search, setSearch] = useState("All");
+
+  console.log(search);
+
+  //set search to user selection, and update state
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="d-flex justify-content-center">
       <Form.Select
         className="w-auto text-start"
         aria-label="Default select example"
+        onChange={handleSearchChange}
+        value={search}
       >
-        <option>Preferences</option>
-        <option value="1">All</option>
-        <option value="2">Most Liked</option>
-        <option value="3">Users</option>
+        <option value="All">All</option>
+        <option value="Most Liked">Most Liked</option>
+        <option value="Users">Users</option>
       </Form.Select>
     </div>
   );
